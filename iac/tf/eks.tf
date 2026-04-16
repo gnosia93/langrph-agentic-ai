c5042e84aa771b3171ec4063e8ad2cd519ac51a7
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-16 18:22:52 +0900

    Update eks.tf

--- iac/tf/eks.tf
+++ iac/tf/eks.tf
@@ -67,7 +67,11 @@ resource "aws_security_group_rule" "ec2_to_eks" {
   source_security_group_id = aws_security_group.instance_sg.id
 }
 
-
+resource "aws_ec2_tag" "eks_cluster_sg_karpenter" {
+  resource_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  key         = "karpenter.sh/discovery"
+  value       = var.cluster_name
+}
 
 ### 추가 끝 ###
 
