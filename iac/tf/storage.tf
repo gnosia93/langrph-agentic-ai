8f4184a46b510a1200a8288c42788b61b239e00c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-18 17:29:08 +0900

    Create storage.tf

---
+++ iac/tf/storage.tf
@@ -0,0 +1,43 @@
+# ============================================
+# Storage Classes
+# ============================================
+
+# 기존 gp2는 default에서 빼기
+resource "kubernetes_annotations" "gp2_not_default" {
+  api_version = "storage.k8s.io/v1"
+  kind        = "StorageClass"
+  metadata {
+    name = "gp2"
+  }
+  annotations = {
+    "storageclass.kubernetes.io/is-default-class" = "false"
+  }
+  force = true
+
+  depends_on = [aws_eks_addon.ebs_csi]
+}
+
+# gp3을 default로 생성
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+  reclaim_policy         = "Delete"
+
+  parameters = {
+    type      = "gp3"
+    encrypted = "true"
+  }
+
+  depends_on = [
+    aws_eks_addon.ebs_csi,
+    kubernetes_annotations.gp2_not_default,
+  ]
+}
