205e261983f2124ad4a0e69873dff2d563451050
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-18 17:26:58 +0900

    Update karpenter.tf

--- iac/tf/karpenter.tf
+++ iac/tf/karpenter.tf
@@ -2,18 +2,6 @@
 # Karpenter Helm Install
 # ============================================
 
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    }
-  }
-}
-
 resource "helm_release" "karpenter" {
   name       = "karpenter"
   repository = "oci://public.ecr.aws/karpenter"
