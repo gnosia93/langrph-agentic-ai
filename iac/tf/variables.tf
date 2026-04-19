0106c543c95f6a635246fcd2d2f96e0cc9c59597
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-19 22:23:13 +0900

    Update variables.tf

--- iac/tf/variables.tf
+++ iac/tf/variables.tf
@@ -25,7 +25,7 @@ variable "gpu_type" {
 variable "system_node_instance_types" {
   description = "EKS system node Group Instance Type "
   type    = list(string)
-  default = ["c7g.2xlarge"]
+  default = ["c7g.4xlarge"]
 }
 
 variable "system_node_ami_type" {
